353d8ef595426662b2199bb318da0435001693b7
jerzy szajner <jerzy.szajner@gmail.com>
2023-11-05 16:09:49 +0100

    Update left column links list
    Remove contents of links list in the left column
    
    Retrieve and store article id into a constant
    
    Find and store article title into a constant
    
    Create and store new HTML link code based on article id and title
    
    Insert new HTML link into the links list in the left column

--- js/script.js
+++ js/script.js
@@ -1,44 +1,86 @@
-'use strict';   // Włącza tryb ścisły JavaScript, który pomaga w wyłapywaniu powszechnych błędów i "cichych" błędów, sprawiając, że JavaScript jest bardziej przewidywalny.
+'use strict';
 
-function titleClickHandler(event) { // Definiujemy funkcję titleClickHandler, która będzie wywoływana, gdy użytkownik kliknie na jeden z linków na stronie. event to obiekt zdarzenia, który zawiera informacje o tym zdarzeniu.
-    event.preventDefault(); // Zapobiega domyślnej akcji przeglądarki. W tym przypadku, kliknięcie na link zwykle prowadziłoby do przejścia do nowej strony lub przewinięcia strony, ale ta linia zapobiega temu.
-    const clickedElement = this;    // this w kontekście nasłuchiwacza zdarzeń odnosi się do elementu, który wywołał zdarzenie - w tym przypadku, do klikniętego linku. Przypisujemy go do stałej clickedElement dla łatwiejszego dostępu.
-    console.log('Link was clicked!'); // Wypisuje informację w konsoli deweloperskiej, że link został kliknięty.
+function titleClickHandler(event) {
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
-    /* [DONE] remove class 'active' from all article links  */
-    // Znajduje wszystkie aktywne linki (linki z klasą 'active') i usuwa z nich klasę 'active'. Służy to "dezaktywacji" linków.
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-    }
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    /* [DONE] add class 'active' to the clicked link */
-    // Dodaje klasę 'active' do klikniętego linku i wyświetla w konsoli ten kliknięty element.
-    clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+  /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-    /* [DONE]remove class 'active' from all articles */
-    // Znajduje wszystkie aktywne artykuły (artykuły z klasą 'active') i usuwa z nich klasę 'active'.
-    const activeArticles = document.querySelectorAll('.posts article.active');
-    for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
-    }
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
-    /* [DONE] get 'href' attribute from the clicked link */
-    // Pobiera wartość atrybutu 'href' z klikniętego linku, co zazwyczaj jest selektorem CSS wskazującym na konkretny artykuł na stronie.
-    const articleSelector = clickedElement.getAttribute('href');
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
 
-    /* [DONE] find the correct article owiadającą wartości atrybutu 'href' klikniętego linku.using the selector (value of 'href' attribute) */
-    // Znajduje na stronie artykuł, który ma id lub klasę odp
-    const targetArticle = document.querySelector(articleSelector);
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle', targetArticle);
 
-    /* [DONE] add class 'active' to the correct article */
-    targetArticle.classList.add('active');
-    console.log('targetArticle:', targetArticle);
+  /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
-// Znajduje wszystkie linki wewnątrz elementu z klasą 'titles'.
+
 const links = document.querySelectorAll('.titles a');
-// Dla każdego linku dodaje nasłuchiwacz zdarzeń, który wywoła funkcję titleClickHandler przy kliknięciu na link.
+
 for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
+  link.style.color = 'green';
 }
+
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+  console.log('generateTitleLinks', generateTitleLinks);
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log('articles:', articles);
+
+  for (const article of articles) {
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log('articleId:', articleId);
+
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log('articleTitle', articleTitle)
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML', linkHTML);
+
+    /* insert link into titleList */
+    titleList.insertAdjacentHTML('beforeend', linkHTML);
+    console.log('Inserted linkHTML into titleList:', linkHTML);
+
+  }
+}
+
+generateTitleLinks();
+
+
+
+
